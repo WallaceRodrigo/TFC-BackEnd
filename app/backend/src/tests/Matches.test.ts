288fb81eb644f2952900e1fee4b02ca2a62b5b2e
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2023-06-15 15:06:13 -0300

    TDD req 15, 16, 17, 18 Matches endpoints

---
+++ app/backend/src/tests/Matches.test.ts
@@ -0,0 +1,113 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import SequelizeMatches from '../database/models/SequelizeMatches';
+import { getAllMatchesMock, inProgressMatchesMock, finishMatchesMock, oneMatchMock, updatedOneMatchMock, validToken } from './mocks/MatchesMocks';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Matches EndPoints Tests', () => {
+  beforeEach(() => {
+    sinon.restore();
+  })
+  describe('GetAll Matches EndPoint', () => {
+    it ('Deve retornar todos as partidas', async () => {
+      sinon.stub(SequelizeMatches, 'findAll').resolves(getAllMatchesMock as any);
+
+  
+      const { status, body } = await chai.request(app).get('/matches');
+  
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal(getAllMatchesMock);
+    })
+  }),
+
+  describe('GetInprogress Matches EndPoint', () => {
+    it ('Deve retornar todas as partidas que estão em progresso', async () => {
+      sinon.stub(SequelizeMatches, 'findAll').resolves(inProgressMatchesMock as any);
+  
+      const { status, body } = await chai.request(app).get('/matches?inProgress=true');
+  
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal(inProgressMatchesMock);
+    })
+
+    it ('Deve retornar todas as partidas que ja terminaram', async () => {
+      sinon.stub(SequelizeMatches, 'findAll').resolves(finishMatchesMock as any);
+  
+      const { status, body } = await chai.request(app).get('/matches?inProgress=false');
+  
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal(finishMatchesMock);
+    })
+  })
+
+  describe('Finish Matches EndPoint', () => {
+    it ('Deve ser possível finalizar uma partida pelo id', async () => {
+      sinon.stub(SequelizeMatches, 'findOne').resolves(oneMatchMock as any);
+      
+      const { status, body } = await chai.request(app)
+      .patch('/matches/1/finish').set('Authorization', validToken);
+      
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal({ message: "Finished" });
+    })
+    
+    it('Deve retornar erro sem um token', async () => {
+      sinon.stub(SequelizeMatches, 'findOne').resolves(oneMatchMock as any);
+
+      const { status, body } = await chai.request(app)
+      .patch('/matches/1/finish')
+
+      expect(status).to.equal(401);
+      expect(body).to.be.deep.equal({ message: "Token not found" });
+    })
+
+    it('Deve retornar erro com um token invalido', async () => {
+      sinon.stub(SequelizeMatches, 'findOne').resolves(oneMatchMock as any);
+  
+      const { status, body } = await chai.request(app)
+      .patch('/matches/1/finish').set('Authorization', 'tokenInvalido')
+
+      expect(status).to.equal(401);
+      expect(body).to.be.deep.equal({ message: "Token must be a valid token" });
+    })
+  })
+
+  describe('Update Matches EndPoint', () => {
+    it ('Deve ser possível atualizar uma partida pelo id', async () => {
+      sinon.stub(SequelizeMatches, 'findOne').resolves(oneMatchMock as any);
+      
+      const { status, body } = await chai.request(app)
+      .patch('/matches/1').set('Authorization', validToken)
+      .send({ homeTeamGoals: 3, awayTeamGoals: 1 });
+      
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal(updatedOneMatchMock);
+    })
+    
+    it('Deve retornar erro sem um token', async () => {
+      sinon.stub(SequelizeMatches, 'findOne').resolves(oneMatchMock as any);
+
+      const { status, body } = await chai.request(app).patch('/matches/1')
+
+      expect(status).to.equal(401);
+      expect(body).to.be.deep.equal({ message: "Token not found" });
+    })
+
+    it('Deve retornar erro com um token invalido', async () => {
+      sinon.stub(SequelizeMatches, 'findOne').resolves(oneMatchMock as any);
+  
+      const { status, body } = await chai.request(app)
+      .patch('/matches/1').set('Authorization', 'tokenInvalido')
+
+      expect(status).to.equal(401);
+      expect(body).to.be.deep.equal({ message: "Token must be a valid token" });
+    })
+  })
+});
---
+++ app/backend/src/tests/mocks/MatchesMocks.ts
@@ -0,0 +1,124 @@
+export const getAllMatchesMock = [
+  {
+    "id": 1,
+    "homeTeamId": 16,
+    "homeTeamGoals": 1,
+    "awayTeamId": 8,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "homeTeam": {
+      "teamName": "São Paulo"
+    },
+    "awayTeam": {
+      "teamName": "Grêmio"
+    }
+  },
+  {
+    "id": 41,
+    "homeTeamId": 16,
+    "homeTeamGoals": 2,
+    "awayTeamId": 9,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+    "homeTeam": {
+      "teamName": "São Paulo"
+    },
+    "awayTeam": {
+      "teamName": "Internacional"
+    }
+  }
+]
+
+export const inProgressMatchesMock = [
+  {
+    "id": 41,
+    "homeTeamId": 16,
+    "homeTeamGoals": 2,
+    "awayTeamId": 9,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+    "homeTeam": {
+      "teamName": "São Paulo"
+    },
+    "awayTeam": {
+      "teamName": "Internacional"
+    }
+  },
+  {
+    "id": 42,
+    "homeTeamId": 6,
+    "homeTeamGoals": 1,
+    "awayTeamId": 1,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+    "homeTeam": {
+      "teamName": "Ferroviária"
+    },
+    "awayTeam": {
+      "teamName": "Avaí/Kindermann"
+    }
+  }
+]
+
+export const finishMatchesMock = [
+  {
+    "id": 1,
+    "homeTeamId": 16,
+    "homeTeamGoals": 1,
+    "awayTeamId": 8,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "homeTeam": {
+      "teamName": "São Paulo"
+    },
+    "awayTeam": {
+      "teamName": "Grêmio"
+    }
+  },
+  {
+    "id": 2,
+    "homeTeamId": 9,
+    "homeTeamGoals": 1,
+    "awayTeamId": 14,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "homeTeam": {
+      "teamName": "Internacional"
+    },
+    "awayTeam": {
+      "teamName": "Santos"
+    }
+  }
+];
+
+export const oneMatchMock = {
+  "id": 1,
+  "homeTeamId": 16,
+  "homeTeamGoals": 1,
+  "awayTeamId": 8,
+  "awayTeamGoals": 1,
+  "inProgress": false,
+  "homeTeam": {
+    "teamName": "São Paulo"
+  },
+  "awayTeam": {
+    "teamName": "Grêmio"
+  }
+};
+
+export const updatedOneMatchMock = {
+  "id": 1,
+  "homeTeamId": 16,
+  "homeTeamGoals": 3,
+  "awayTeamId": 8,
+  "awayTeamGoals": 1,
+  "inProgress": false,
+  "homeTeam": {
+    "teamName": "São Paulo"
+  },
+  "awayTeam": {
+    "teamName": "Grêmio"
+  }
+};
+
+export const validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsInJvbGUiOiJhZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwicGFzc3dvcmQiOiIkMmEkMDgkeGkuSHhrMWN6QU8wblpSLi5CMzkzdTEwYUVEMFJRMU4zUEFFWFE3SHh0TGpLUEVaQnUuUFciLCJpYXQiOjE2ODY3NzgxODF9.THSqwh2nIubf3EuOSx01B0b_unSZTnPWJ_hxEFbqyIM";
